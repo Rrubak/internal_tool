1421766ef01315e3ae3ed7ee2bb6a715717c6652
Rrubak <14cse11@gmail.com>
2016-07-30 17:11:48 +0530

    added db

---
+++ project1/project1/db/test.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 30, 2016 at 01:35 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_password`
+--
+
+CREATE TABLE IF NOT EXISTS `user_password` (
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_password`
+--
+
+INSERT INTO `user_password` (`username`, `password`) VALUES
+('sudhakar', '1234'),
+('rubak', 'rubak1');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ project1/project1/index.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>
+		 	LOG IN
+		 </title>
+		<link href="css/index_css.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="login">
+			<?php 
+					error_reporting(0);
+					session_start();
+	  			function Verify(){
+			  		if($_SESSION["verify"]=="verify"){
+			  			echo '<a style="color:#1D82ED">wrong password</a>';
+			  			session_unset();
+			  			session_destroy();
+			  		} 
+		  		}
+		  		Verify();
+	  		?>
+	  		<h1>Application Login</h1>
+	  		<form method="post" action="app/login_validate.php">
+		  		<input placeholder="Username" name="username" autofocus required>
+		  		<input type="password" name="password" placeholder="Password" required>
+		  			<a >Forgot your Password?</a>
+		  		<input type="submit" class="btn" name="submit" value="log in">
+	  		</form>
+		</div>
+		<div class="message">
+			Error. Enter a valid email address.
+		</div>
+	</body>
+</html>
